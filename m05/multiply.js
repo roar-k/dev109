0c797f25a1c3b23155a21609140ad6cdd539017f
Ben <124755801+roar-k@users.noreply.github.com>
2025-05-08 19:53:42 -0700

    Update multiply.js

--- m05/multiply.js
+++ m05/multiply.js
@@ -1,21 +1,15 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var operator = 'multiplication'; // Type of calculation
+var i = 1; // Set counter to 1
+var msg =  '<h2>Multiplication Table</h2>'; // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
+// Asks the user to enter a number between 1 to 10
+var table = parseInt(prompt("Enter a from 0 to 10: "));
+                            
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
