1825f19e9e5bdb38eb654da9b9a101dc317a024a
Ben <124755801+roar-k@users.noreply.github.com>
2025-05-08 20:00:03 -0700

    Update multiply.js

--- m05/multiply.js
+++ m05/multiply.js
@@ -4,6 +4,11 @@ var msg =  '<h2>Multiplication Table</h2>'; // Message
 
 // Asks the user to enter a number between 1 to 10
 var table = parseInt(prompt("Enter a from 0 to 10: "));
+
+// Changes null or NaN to 0
+if (table == NaN || table == null) {
+  table = 0;
+}
                             
   // Do multiplication
   while (i < 11) {
