6faf3bfda73dbfe5d1b7608d40670dc1a85ff377
Ben <124755801+roar-k@users.noreply.github.com>
2025-06-05 13:29:52 -0700

    Update addElement.js

--- m09/js/addElement.js
+++ m09/js/addElement.js
@@ -1,14 +1,51 @@
-// Create a new element and store it in a variable.
-var newEl = document.createElement('li');
+function addItem() {
+  var input = document.getElementById('newItem');
+  var value = input.value;
+  var message = document.getElementById('message');
 
-// Create a text node and store it in a variable.
-var newText = document.createTextNode('quinoa');
+  while (true) {
+    try {
+      // Catches null entry value
+      if (!value) {
+        throw "Null input";
+      }
 
-// Attach the new text node to the new element.
-newEl.appendChild(newText);
+      // Create a new element and store it in a variable.
+      var newEl = document.createElement('li');
 
-// Find the position where the new element should be added.
-var position = document.getElementsByTagName('ul')[0];
+      // Create a text node and store it in a variable.
+      var newText = document.createTextNode('value');
 
-// Insert the new element into its position.
-position.appendChild(newEl);
+      // Attach the new text node to the new element.
+      newEl.appendChild(newText);
+
+      // Find the position where the new element should be added.
+      var position = document.getElementsByTagName('ul')[0];
+
+      // Insert the new element into its position.
+      position.appendChild(newEl);
+    }
+
+    // Catches errors and writes an error to the console
+    catch (e) {
+      console.log(e);
+    }
+
+    // Writes an error message that the entry was empty
+    finally {
+      if (!value) {
+        message.textContent = "Entry was empty";
+        break;
+      }
+    }
+    break;
+  }
+}
+      
+
+
+      
+
+      
+
+      
