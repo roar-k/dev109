8d17cd2ad183b19461bdb4efb8f79b2cc1b5ddd6
Ben <124755801+roar-k@users.noreply.github.com>
2025-06-05 14:17:24 -0700

    Update validate.js

--- a4/js/validate.js
+++ a4/js/validate.js
@@ -1,15 +1,34 @@
-function isValid() {
-    if (firstName() //&&
-       // lastName()
-    )
-    return true;
-    else
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("FirstName").addEventListener('blur', firstName, false);
+});
+
+function isValid(event) {
+    // Clear submit error
+    document.getElementById("submiterror").innerHTML = "";
+
+    // Validate all fields
+    if (firstName() &&
+        // lastName() &&
+        // email() &&
+        // phone() &&
+        // username() &&
+        // password() &&
+        // address() &&
+        // city() &&
+        // state() &&
+        // country() &&
+        // zipCode()
+    ) {
+        return true;
+    }
+        
+    else {
         document.getElementById("submiterror").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
         event.preventDefault();
         return false;
+    }
 }
 
-FirstName.addEventListener('blur', firstName, false);
 function firstName(){
     //1) Create variable
     var validFirstname=false;
@@ -19,16 +38,20 @@ function firstName(){
     var errorMessages = "";
 
     //3) Do validation
-    if (firstname==="null" || firstname==="" || firstname.length > 20 ) {
+    if (firstname === "" || firstname === "" || firstname.length > 20 ) {
         errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
         console.log("First name invalid — length")
-        } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
-            console.log("First name invalid — bad characters")
-        } else {
-                validFirstname = true;
-                console.log("First name valid")
-        };
+    }
+        
+    else if (!firstname.match("^[a-zA-Z ,.'-]+$")) {
+        errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+        console.log("First name invalid — bad characters");
+    }
+        
+    else {
+        validFirstname = true;
+        console.log("First name valid");
+    }
 
     //4) Send error message to HTML
     document.getElementById("fname").innerHTML = errorMessages;
