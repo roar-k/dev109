c2c4783347fae5cb385f024baa4e3a19c0ea29fb
Ben <124755801+roar-k@users.noreply.github.com>
2025-06-12 00:37:45 -0700

    Update slideshow.js

--- a5/js/slideshow.js
+++ a5/js/slideshow.js
@@ -1,14 +1,14 @@
 var myImages =["https://i.pinimg.com/originals/bf/44/fa/bf44fa8743e026c7d3955dd28a4d32fa.jpg", 
-															"https://wallup.net/wp-content/uploads/2015/12/124083-skyscraper-New_York_City-city-landscape.jpg", 
-															"https://wallpaperaccess.com/full/33929.jpg", 
-															"https://th.bing.com/th/id/OIP.ZRQLcRGiP0kn5iXqv1FZVgHaEo?rs=1&pid=ImgDetMain", 
-															"https://jakadatoursegypt.com/wp-content/uploads/2022/07/Khafre-pyramid.jpg"];
+							 "https://wallup.net/wp-content/uploads/2015/12/124083-skyscraper-New_York_City-city-landscape.jpg", 
+							 "https://wallpaperaccess.com/full/33929.jpg", 
+							 "https://th.bing.com/th/id/OIP.ZRQLcRGiP0kn5iXqv1FZVgHaEo?rs=1&pid=ImgDetMain", 
+							 "https://jakadatoursegypt.com/wp-content/uploads/2022/07/Khafre-pyramid.jpg"];
 
 var captionImages =["Beautiful scenery of Japan with Mount Fuji in the background and Sakura Trees in the front", 
-																				"Beatiful scenery of New York City at night with the city buildings reflecting in the water", 
-																				"Beautiful scenery of the Great Wall of China under the sunset", 
-																				"Beautiful scenery of the Eiffel Tower in Paris with the sunset in the background", 
-																				"Beautiful scenery of Egypt with the Pyramid of Giza in the background under the bright sun"];
+										"Beatiful scenery of New York City at night with the city buildings reflecting in the water", 
+										"Beautiful scenery of the Great Wall of China under the sunset", 
+										"Beautiful scenery of the Eiffel Tower in Paris with the sunset in the background", 
+										"Beautiful scenery of Egypt with the Pyramid of Giza in the background under the bright sun"];
 
 var index = 0; 
 
